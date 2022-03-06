aa82b84b0fc7050f5fb5b0f8c8fe9501e33ac4af
Nathan Batchelder <natebatchelder@gmail.com>
2022-03-06 12:53:04 -0800

    updates to db

--- db/db.sql
+++ db/db.sql
@@ -1,5 +1,14 @@
--- todo: clear db/tables
+-- !clear db/tables & create db/tables & use db/tables
+-- clear, create, use employee tracker database 
+
+-- todo: drop db/tables
+-- clears the database if it exists
+DROP DATABASE IF EXISTS employee-tracker-db;
 
 -- todo: create db/tables
+-- creates the employee tracker db
+CREATE DATABASE employee-tracker-db;
 
 -- todo: use above db
+-- uses the employee tracker database
+USE employee-tracker-db;
