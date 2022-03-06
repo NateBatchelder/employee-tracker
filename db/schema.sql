77bc31cef2736015288e08cd618206fe6319c5de
Nathan Batchelder <natebatchelder@gmail.com>
2022-03-06 12:48:43 -0800

    updates to db schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,52 @@
 -- todo: drop tables
 
 -- todo: drop departments
+DROP TABLE IF EXISTS departments;
 -- todo: drop roles
+DROP TABLE IF EXISTS roles;
 -- todo: drop employess
+DROP TABLE IF EXISTS employees;
 
--- create departments
+-- *create table departments
+-- sales
+-- engineering
+-- finance
+-- legal
+CREATE TABLE departments (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
 
--- create roles
+-- *create table roles
+-- salesperson
+-- engineer
+-- accountant
+-- lawyer
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary INTEGER NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
+);
 
--- create employees
+-- *create table employees
+-- id
+-- first_name
+-- last_name
+-- role_id
+-- manager_id
+-- foreign key (role_id) references roles (id)
+    -- on delete set null
+-- foreign key (manager_id) references employees (id)
+    -- on delete set null
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+);
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,59 @@
 -- todo: seed data for departments, roles, employees
 
--- seed data for department
+-- *seed data for department
+-- sales
+-- engineering
+-- finance
+-- legal
+    -- id
+    -- name
+INSERT INTO departments (id, name)
+VALUES
+    (1, "sales"),
+    (2, "engineering"),
+    (3, "finance"),
+    (4, "legal");
 
--- seed data for roles
+-- *seed data for roles
+-- title
+-- salary
+-- department_id
+    -- 1 salesperson
+    -- 2 engineer
+    -- 3 accountant
+    -- 4 lawyer
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ("salesperson", 50000, 1),
+    ("engineer", 60000, 2),
+    ("accountant", 40000, 3),
+    ("lawyer", 80000, 4);
 
--- seed data for employees
+-- *seed data for employees
+-- first_name
+-- last_name
+-- role_id
+-- manager_id
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ("John", "Doe", 1, NULL),
+    ("Jane", "Doe", 2, NULL),
+    ("Joe", "Doe", 3, NULL),
+    ("Jill", "Doe", 4, NULL),
+    ("John", "Smith", 1, 1),
+    ("Jane", "Smith", 2, 1),
+    ("Joe", "Smith", 3, 1),
+    ("Jill", "Smith", 4, 1),
+    ("John", "Williams", 1, 2),
+    ("Jane", "Williams", 2, 2),
+    ("Joe", "Williams", 3, 2),
+    ("Jill", "Williams", 4, 2),
+    ("John", "Jones", 1, 3),
+    ("Jane", "Jones", 2, 3),
+    ("Joe", "Jones", 3, 3),
+    ("Jill", "Jones", 4, 3),
+    ("John", "Brown", 1, 4),
+    ("Jane", "Brown", 2, 4),
+    ("Joe", "Brown", 3, 4),
+    ("Jill", "Brown", 4, 4);
 
