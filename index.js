f1f876c7d0a5b025933860a5150585e331011975
Nathan Batchelder <natebatchelder@gmail.com>
2022-03-06 12:15:30 -0800

    adding pseudo code outline - index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+// todo: require 
+
+// inquirer
+// mysql2
+// console.table
+// database connection
+
+// access to database
+
+// error handling
+
+// menu 
+
+// show employees
+
+// show departments
+
+// show roles
+
+// add employee
+
+// add department
+
+// add role
+
+// update role
+
+// update manager
+
+// delete employee
+
+// delete department
+
+// delete role
+
+
+// view employee by department
+
+// view aggregate budget by department
